1a073d6d569db5ff2427031aa61db97576ca0c10
davidlefebvre19 <davidlfbvr19@gmail.com>
2024-07-21 12:37:37 +0200

    mt poc

--- v2/mt_test/anim_controller_poc.py
+++ v2/mt_test/anim_controller_poc.py
@@ -14,31 +14,31 @@ class StoppableThread(threading.Thread):
     def run(self):
         with self.lock:
             self.task(self.stop_event, self.name)  # Appel de la tâche spécifiée
+        self.stop_event.clear()
 
 # Fonction pour démarrer et arrêter les threads séquentiellement
 def run_threads_sequentially():
     lock = threading.Lock()
     stop_event = threading.Event()
 
-    # Création des threads avec des tâches spécifiques et des durées
-    task_a_thread = StoppableThread("Thread A", lock, stop_event, task_a, 5)  # 5 secondes pour tâche A
-    task_b_thread = StoppableThread("Thread B", lock, stop_event, task_b, 5)  # 5 secondes pour tâche B
-    task_c_thread = StoppableThread("Thread C", lock, stop_event, task_c, 5)  # 5 secondes pour tâche C
+    # Création des instances de threads avec des tâches spécifiques
+    threads = [
+        StoppableThread("Thread A", lock, stop_event, task_a),
+        StoppableThread("Thread B", lock, stop_event, task_b),
+        StoppableThread("Thread C", lock, stop_event, task_c)
+    ]
 
-    # Démarrage de task_a
-    task_a_thread.start()
-    task_a_thread.join()  # Attendre la fin de task_a
+    for thread in threads:
+        # stop_event.clear()  # Réinitialiser l'événement d'arrêt
+        thread.start()
+        time.sleep(5)  # Laisser le thread fonctionner pendant 5 secondes
+        stop_event.set()  # Signal pour arrêter le thread
+        thread.join()  # Attendre que le thread se termine proprement
 
-    # Démarrage de task_b
-    task_b_thread.start()
-    time.sleep(3)  # Laisser task_b fonctionner pendant 3 secondes
-    stop_event.set()  # Activer stop_event pour interrompre task_b
-    task_b_thread.join()  # Attendre que task_b se termine
+    for count, thread in enumerate(threads):
+        pass
 
-    # Démarrage de task_c
-    task_c_thread.start()
-    task_c_thread.join()  # Attendre que task_c se termine
 
 run_threads_sequentially()
 
-print("Tous les threads ont été exécutés et arrêtés.")
+print("Tous les threads ont été exécutés séquentiellement.")
