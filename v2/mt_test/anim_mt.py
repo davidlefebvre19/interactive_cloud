8e18be419e73440a33e231d43301074af43e0834
davidlefebvre19 <davidlfbvr19@gmail.com>
2024-07-21 21:16:34 +0200

    mt poc final ???

--- v2/mt_test/anim_mt.py
+++ v2/mt_test/anim_mt.py
@@ -16,22 +16,21 @@ def r(stop_event, duration):
     start_time = time.time()
     while not stop_event.is_set() and (time.time() - start_time) < duration:
         print("LED strip en mode rainbow...")
-        print(time.time() - start_time)
         time.sleep(1)
     print("Animation rainbow terminée")
 
 def c(stop_event, duration):
     print(f"Animation chill démarrée pour {duration} secondes")
     start_time = time.time()
-    while not stop_event.is_set() or (time.time() - start_time) < duration:
+    while not stop_event.is_set() and (time.time() - start_time) < duration:
         print("LED strip en mode chill...")
-        time.sleep(0.1)
+        time.sleep(1)
     print("Animation chill terminée")
 
 def t(stop_event, duration):
     print(f"Animation thunder démarrée pour {duration} secondes")
     start_time = time.time()
-    while not stop_event.is_set() or (time.time() - start_time) < duration:
+    while not stop_event.is_set() and (time.time() - start_time) < duration:
         print("LED strip en mode thunder...")
-        time.sleep(0.1)
+        time.sleep(1)
     print("Animation thunder terminée")
