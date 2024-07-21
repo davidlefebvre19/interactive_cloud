4f209b8f0769bab3cf4e68e3bbf00752b06ac6c3
davidlefebvre19 <davidlfbvr19@gmail.com>
2024-07-21 21:55:24 +0200

    mt poc final ???

--- v2/mt_test/client_led.py
+++ v2/mt_test/client_led.py
@@ -19,14 +19,14 @@ def handle_cmd(task_queue, command, duration, stop_event):
     elif command == "c":
         task_queue.put((c, duration))
     elif command == "t":
-        stop_event.set()
+        #stop_event.set()
         # vider les commandes précédentes
         print("Commande thunder recue, suppression des commandes restantes !!!")
         i = 0
         while not task_queue.empty():
             task_queue.get()
             i += 1
-        stop_event.clear()
+        #stop_event.clear()
         print(str(i) + " commandes supprimées")
         # configurer commande thunder
         task_queue.put((t, duration))
