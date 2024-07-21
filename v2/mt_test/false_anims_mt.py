2fdec08e6be574c36e52a955fb31b5c1e89ba473
davidlefebvre19 <davidlfbvr19@gmail.com>
2024-07-21 13:47:12 +0200

    mt poc

--- v2/mt_test/false_anims_mt.py
+++ v2/mt_test/false_anims_mt.py
@@ -6,8 +6,8 @@ def task_a(stop_event, name, duration):
     while not stop_event.is_set() and (time.time() - start_time) < duration:
         print(f"{name} en cours d'exécution...")
         time.sleep(1)
-    if not stop_event.is_set:
-        print("Thread stopped by duration NOT stop event")
+    if stop_event.is_set:
+        print("Thread stopped by stop event")
     print(f"{name} arrêté proprement")
 
 def task_b(stop_event, name, duration):
