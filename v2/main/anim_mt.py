0bbc5fea160f456853d91735a9988be3029476ee
davidlefebvre19 <davidlfbvr19@gmail.com>
2024-07-22 16:49:15 +0200

    connecting mt led controller to main server using socket lib

--- v2/main/anim_mt.py
+++ v2/main/anim_mt.py
@@ -7,13 +7,24 @@ import random
 PIN = 12
 NUM_LEDS = 300
 BRIGHTNESS = 255
+
+# CHILL
 MIN_BRIGHTNESS = int(BRIGHTNESS * 0.20)  # 20% de la luminosité maximale
 HALO_SIZE = 50
 DELAY = 0.05
 
-# Couleurs de bleu
 BLUE_DARK = Color(0, 0, MIN_BRIGHTNESS)
 BLUE_LIGHT = Color(0, 0, int(BRIGHTNESS * 0.50))  # Bleu clair avec 50% de la luminosité maximale
+
+# RAIN
+BRIGHTNESS = 100
+FADE_OUT_RATE = 5
+#    Couleur de la pluie
+RAIN_COLOR = Color(0, 0, 255)
+
+#    Tableaux pour suivre l'état de chaque LED
+led_brightness = [0 for _ in range(NUM_LEDS)]
+is_raining = [False for _ in range(NUM_LEDS)]
 
 # Initialisation de la bande de LEDs
 strip = Adafruit_NeoPixel(NUM_LEDS, PIN, 800000, 10, False, BRIGHTNESS)
@@ -29,7 +40,7 @@ class StoppableThread(threading.Thread):
     def run(self):
         self.task(self.stop_event, self.duration)
 
-##################################### RAIN ###########################################
+########################################### CHILL
 
 def set_background():
     """Allume le fond avec une couleur bleu foncé ou bleu clair."""
@@ -40,7 +51,7 @@ def set_background():
             strip.setPixelColor(i, BLUE_LIGHT)
     strip.show()
 
-def r(stop_event, duration):
+def c(stop_event, duration):
     print(f"Animation rain démarrée pour {duration} secondes")
     start_time = time.time()
 
@@ -69,14 +80,43 @@ def r(stop_event, duration):
         # Avancer les positions des halos
         position1 = (position1 + 1) % NUM_LEDS
         position2 = (position2 + 1) % NUM_LEDS
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(0, 0, 0))
+    strip.show()
     print("Animation rain terminée")
 
-def c(stop_event, duration):
+########################################### RAIN
+
+def update_leds(strip):
+    """ Mettre à jour l'état de la bande LED """
+    for i in range(NUM_LEDS):
+        if led_brightness[i] > 0:
+            led_brightness[i] = max(led_brightness[i] - FADE_OUT_RATE, 0)
+            strip.setPixelColor(i, Color(0, 0, led_brightness[i]))
+            if led_brightness[i] == 0:
+                is_raining[i] = False
+        else:
+            strip.setPixelColor(i, Color(0, 0, 0))
+    strip.show()
+
+def rain_drop(strip):
+    """ Créer une goutte de pluie """
+    start_led = random.randint(0, strip.numPixels() - 1)
+    if not is_raining[start_led]:
+        led_brightness[start_led] = 255  # Luminosité maximale pour commencer
+        is_raining[start_led] = True
+
+def r(stop_event, duration):
     print(f"Animation chill démarrée pour {duration} secondes")
     start_time = time.time()
     while not stop_event.is_set() and (time.time() - start_time) < duration:
         print("LED strip en mode chill...")
-        time.sleep(1)
+        rain_drop(strip)
+        update_leds(strip)
+        time.sleep(0.1)
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(0, 0, 0))
+    strip.show()
     print("Animation chill terminée")
 
 def t(stop_event, duration):
