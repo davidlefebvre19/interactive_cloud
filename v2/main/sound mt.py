2a2b26c117d0677eb936966717e9cf43fe97df89
davidlefebvre19 <davidlfbvr19@gmail.com>
2024-07-31 14:02:36 +0200

    sound controller

---
+++ v2/main/sound mt.py
@@ -0,0 +1,43 @@
+import threading
+import time
+import pygame
+from pydub import AudioSegment
+
+fade_out_duration = 2000
+class StoppableThread(threading.Thread):
+    def __init__(self, task, stop_event, duration, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.task = task
+        self.stop_event = stop_event
+        self.duration = duration
+
+    def run(self):
+        self.task(self.stop_event, self.duration)
+
+def run_music(stop_event, duration, audio_file):
+    # Convertir et jouer le fichier audio
+    song = AudioSegment.from_file(audio_file)
+    pygame.mixer.init(frequency=song.frame_rate)
+    pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.play(-1)
+
+    start_time = time.time()
+
+    while not stop_event.is_set() and (time.time() - start_time) < duration:
+        continue
+
+    # Fade-out
+    pygame.mixer.music.fadeout(fade_out_duration)
+
+    # Attendre la fin du fade-out avant d'arrêter la musique
+    pygame.time.wait(fade_out_duration)
+    pygame.mixer.music.stop()
+
+def c(stop_event, duration):
+    run_music(stop_event, "./sounds/wind.mp3", duration)
+
+def r(stop_event, duration):
+    run_music(stop_event, "./sounds/rain.mp3", duration)
+
+def t(stop_event, duration):
+    run_music(stop_event, "./sounds/thunder.mp3", duration)
---
+++ v2/main/sound_controller.py
@@ -0,0 +1,92 @@
+import threading
+from anim_mt import StoppableThread, r, c, t
+import queue
+import socket
+
+host = '127.0.0.1'
+port = 65432
+
+
+def cmdlistener(cmd_queue):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        print("client connected to server : listening")
+        while True:
+            data = s.recv(1024)
+
+            if not data:
+                break
+
+            if len(data.decode().split()) == 2:
+                #print(data.decode())
+                #print(type(data.decode()))
+                cmd_queue.put(data.decode())
+
+
+def handle_cmd(task_queue, command, duration):
+    if command == "r":
+        task_queue.put((r, duration))
+    elif command == "c":
+        task_queue.put((c, duration))
+    elif command == "t":
+        #stop_event.set()
+        # vider les commandes précédentes
+        print("Commande thunder recue, suppression des commandes restantes !!!")
+        i = 0
+        while not task_queue.empty():
+            task_queue.get()
+            i += 1
+        #stop_event.clear()
+        print(str(i) + " commandes supprimées")
+        # configurer commande thunder
+        task_queue.put(("stop",))
+        task_queue.put((t, duration))
+
+
+def cmdhandler(cmd_queue, task_queue):
+    print("cmd handler start")
+    while True:
+        cmd = cmd_queue.get()
+        # print(cmd)
+        command, duration = cmd.split()
+        handle_cmd(task_queue, command, duration)
+
+
+def taskexecutor(task_queue, stop_event):
+    print("executor start")
+    CT = None
+    while True:
+        task_data = task_queue.get()
+        if task_data[0] == "stop":
+            if CT and CT.is_alive():
+                print("interuption de l'animation actuelle !")
+                stop_event.set()
+                CT.join()
+                stop_event.clear()
+        else:
+            if CT and CT.is_alive():
+                CT.join()
+            CT = StoppableThread(task=task_data[0], stop_event=stop_event, duration=int(task_data[1]))
+            CT.start()
+
+
+def main():
+    stop_event = threading.Event()
+    cmd_queue = queue.Queue()
+    task_queue = queue.Queue()
+
+    listener_thread = threading.Thread(target=cmdlistener, args=(cmd_queue,))
+    handler_thread = threading.Thread(target=cmdhandler, args=(cmd_queue, task_queue,))
+    executor_thread = threading.Thread(target=taskexecutor, args=(task_queue, stop_event,))
+
+    listener_thread.start()
+    handler_thread.start()
+    executor_thread.start()
+
+    listener_thread.join()
+    handler_thread.join()
+    executor_thread.join()
+
+
+if __name__ == "__main__":
+    main()
