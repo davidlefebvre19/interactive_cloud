d9aedd5d612d58b861ac87e4138e3cf4c8fe0a01
davidlefebvre19 <davidlfbvr19@gmail.com>
2024-10-03 15:15:04 +0200

    fine tuning

--- v2/main/server.py
+++ v2/main/server.py
@@ -80,7 +80,7 @@ def sensing_and_sending(max_dist, conn, addr):
             previous_dist = distance
             pass
         else:
-            duration = random.randint(10, 20)
+            duration = random.randint(15, 25)
             if abs(int(previous_dist - distance)) >= 15 and distance <= max_dist:
                 # Movement detected !
                 print("movement detected -> triggering thunder mecanism !")
